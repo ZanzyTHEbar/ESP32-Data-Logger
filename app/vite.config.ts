e728ca4c2c91f87a8ae127f99b25fe2acd6b59f7
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2022-11-21 16:38:43 +0000

    minor update

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, './src'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@pages': path.resolve(__dirname, './src/pages'),
       '@config': path.resolve(__dirname, './src/config'),
       '@styles': path.resolve(__dirname, './src/styles'),
       '@redux': path.resolve(__dirname, './src/redux'),
